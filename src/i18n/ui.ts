5ae21936cc30145babeb1e8b558ab4d632e8a9e1
soumame <sou.tokumaru@gmail.com>
2023-04-08 19:53:42 +0800

    多言語対応
    英語、日本語、豆語に対応。
    https://docs.astro.build/ja/recipes/i18n/#translate-ui-strings
    を参考に制作したで
    後は、多言語化にあたって../の相対パスだとバグるので、それの修正とか。

---
+++ src/i18n/ui.ts
@@ -0,0 +1,34 @@
+export const languages = {
+    en: 'English',
+    ja: '日本語',
+    mame: '🫘Bean/まめ',
+  };
+  
+  export const defaultLang = 'ja';
+  
+  export const ui = {
+    en: {
+      'nav.home': 'Home',
+      'nav.AboutPage': 'About This site',
+      'nav.Twitter': 'Twitter',
+      'nav.Outstndrs': 'Team Outstndrs',
+      'nav.Discord': 'Join Discord',
+      'nav.soto': 'So Tokumaru',
+    },
+    ja: {
+      'nav.home': 'ホーム',
+      'nav.AboutPage': 'このページについて',
+      'nav.Twitter': 'Twitter',
+      'nav.Outstndrs': 'チーム逸般人',
+      'nav.Discord': 'Discordに参加',
+      'nav.soto': '得丸創生',
+    },
+    mame: {
+      'nav.home': 'まめぇ',
+      'nav.AboutPage': '豆豆',
+      'nav.Twitter': 'まめっめー',
+      'nav.Outstndrs': 'まめ豆豆',
+      'nav.Discord': 'まーめまめ',
+      'nav.soto': 'そうまめ',
+    },
+  } as const;
