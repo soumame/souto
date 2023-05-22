df5f5a760d3918be6a4f57436961fdc8b50b9cde
soumame <sou.tokumaru@gmail.com>
2023-05-22 11:47:55 +0800

    GAの追加
    アナリティクスは大事！

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,17 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import partytown from "@astrojs/partytown";
+
 
 export default defineConfig({
   integrations: [
     tailwind(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
   ]
 });
